cf55ff504d767196b6334466937b2c160d93dabe
akgoal <akishingoal@yandex.ru>
2016-05-10 00:43:21 +0300

    TestProjectWithSpring upd

--- TestProjectWithSpring/src/testproject/server/bookservice/BookServiceDBImpl.java
+++ TestProjectWithSpring/src/testproject/server/bookservice/BookServiceDBImpl.java
@@ -2,9 +2,6 @@ package testproject.server.bookservice;
 
 import java.util.ArrayList;
 
-import org.hibernate.HibernateException;
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -12,7 +9,7 @@ import com.google.gwt.user.server.rpc.RemoteServiceServlet;
 
 import testproject.client.BookService;
 import testproject.client.objects.Book;
-import testproject.server.bookservice.dao.BooksDAO;
+import testproject.server.bookservice.dao.BooksDAOI;
 import testproject.server.bookservice.datasets.BooksDataSet;
 
 /**
@@ -24,78 +21,46 @@ import testproject.server.bookservice.datasets.BooksDataSet;
 public class BookServiceDBImpl extends RemoteServiceServlet implements BookService {
 
 	@Autowired
-	private SessionFactory sessionFactory;
+	private BooksDAOI dao;
 
 	@Override
-	public ArrayList<Book> sendServer() throws Exception{
+	public ArrayList<Book> sendServer() {
 		ArrayList<Book> books = new ArrayList<>();
-		try {
-			Session session = sessionFactory.openSession();
-			BooksDAO dao = new BooksDAO(session);
-			ArrayList<BooksDataSet> dataSets = dao.getAllBooks();
-			for (BooksDataSet bds : dataSets)
-			{
-				books.add(convertToBook(bds));
-			}
-			session.close();
-		} catch (HibernateException e) {
-			e.printStackTrace();
-			throw e;
+
+		ArrayList<BooksDataSet> dataSets = dao.getAllBooks();
+		for (BooksDataSet bds : dataSets) {
+			books.add(convertToBook(bds));
 		}
+
 		return books;
 	}
 
 	@Override
-	public ArrayList<Book> findBooksByAuthorBook(long callInput) throws Exception{
+	public ArrayList<Book> findBooksByAuthorBook(long callInput) {
 		ArrayList<Book> books = new ArrayList<>();
-		try {
-			Session session = sessionFactory.openSession();
-			BooksDAO dao = new BooksDAO(session);
-			ArrayList<BooksDataSet> dataSets = dao.getBooksByAuthorId(callInput);
-			for (BooksDataSet bds : dataSets)
-			{
-				books.add(convertToBook(bds));
-			}
-			session.close();
-		} catch (HibernateException e) {
-			e.printStackTrace();
-			throw e;
-		}
+		ArrayList<BooksDataSet> dataSets = dao.getBooksByAuthorId(callInput);
+		for (BooksDataSet bds : dataSets)
+			books.add(convertToBook(bds));
 		return books;
 	}
 
 	@Override
-	public ArrayList<Book> findBooksByGenreBook(long callInput) throws Exception {
+	public ArrayList<Book> findBooksByGenreBook(long callInput) {
 		ArrayList<Book> books = new ArrayList<>();
-		try {
-			Session session = sessionFactory.openSession();
-			BooksDAO dao = new BooksDAO(session);
-			ArrayList<BooksDataSet> dataSets = dao.getBooksByGenreId(callInput);
-			for (BooksDataSet bds : dataSets)
-			{
-				books.add(convertToBook(bds));
-			}
-			session.close();
-		} catch (HibernateException e) {
-			e.printStackTrace();
-			throw e;
+
+		ArrayList<BooksDataSet> dataSets = dao.getBooksByGenreId(callInput);
+		for (BooksDataSet bds : dataSets) {
+			books.add(convertToBook(bds));
 		}
+
 		return books;
 	}
-	
+
 	@Override
-	public Book selectBook(long callInput) throws Exception{
-		Book book = new Book();
-		try {
-			Session session = sessionFactory.openSession();
-			BooksDAO dao = new BooksDAO(session);
-			BooksDataSet dataSet = dao.getBookById(callInput);
-			book = convertToBook(dataSet);
-			session.close();
-		} catch (HibernateException e) {
-			e.printStackTrace();
-			throw e;
-		}
+	public Book selectBook(long callInput) {
+		BooksDataSet dataSet = dao.getBookById(callInput);
+		Book book = convertToBook(dataSet);
+
 		return book;
 	}
 
@@ -106,17 +71,11 @@ public class BookServiceDBImpl extends RemoteServiceServlet implements BookServi
 		back.setTitle(" -> " + callInput.getTitle());
 		return back;
 	}
-	
+
 	private Book convertToBook(BooksDataSet booksDataSet) {
-		return new Book(
-				booksDataSet.getId(), 
-				booksDataSet.getAuthor().getName(),
-				booksDataSet.getAuthor().getId(),
-				booksDataSet.getTitle(),
-				booksDataSet.getGenre().getName(),
-				booksDataSet.getGenre().getId(),
-				booksDataSet.getImgUrl()
-				);
+		return new Book(booksDataSet.getId(), booksDataSet.getAuthor().getName(), booksDataSet.getAuthor().getId(),
+				booksDataSet.getTitle(), booksDataSet.getGenre().getName(), booksDataSet.getGenre().getId(),
+				booksDataSet.getImgUrl());
 	}
-	
+
 }
--- TestProjectWithSpring/src/testproject/server/bookservice/dao/BooksDAO.java
+++ TestProjectWithSpring/src/testproject/server/bookservice/dao/BooksDAO.java
@@ -3,10 +3,12 @@ package testproject.server.bookservice.dao;
 import java.util.ArrayList;
 
 import org.hibernate.Criteria;
-import org.hibernate.Session;
+import org.hibernate.SessionFactory;
 import org.hibernate.criterion.MatchMode;
 import org.hibernate.criterion.Restrictions;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import testproject.server.bookservice.datasets.AuthorsDataSet;
 import testproject.server.bookservice.datasets.BooksDataSet;
@@ -17,33 +19,33 @@ import testproject.server.bookservice.datasets.GenresDataSet;
  */
 
 @SuppressWarnings("unchecked")
-public class BooksDAO implements BooksDAOI {
-	private Session session;
-
-	public BooksDAO(Session session) {
-		this.session = session;
-	}
+@Repository
+@Transactional
+public class BooksDAO implements BooksDAOI{
+	
+	@Autowired
+	private SessionFactory sessionFactory;
 
 	@Override
 	public ArrayList<BooksDataSet> findBooksByTitle(String title) {
-		Criteria criteria = session.createCriteria(BooksDataSet.class);
+		Criteria criteria = sessionFactory.getCurrentSession().createCriteria(BooksDataSet.class);
 		criteria.add(Restrictions.ilike("title", title, MatchMode.ANYWHERE));
 		return (ArrayList<BooksDataSet>) criteria.list();
 	}
 
 	@Override
 	public ArrayList<BooksDataSet> getAllBooks() {
-		return (ArrayList<BooksDataSet>) session.createCriteria(BooksDataSet.class).list();
+		return (ArrayList<BooksDataSet>) sessionFactory.getCurrentSession().createCriteria(BooksDataSet.class).list();
 	}
 
 	@Override
 	public void addBook(BooksDataSet book) {
-		session.save(book);
+		sessionFactory.getCurrentSession().save(book);
 	}
 
 	@Override
 	public ArrayList<BooksDataSet> getBooksByAuthorId(long authorId) {
-		Criteria criteria = session.createCriteria(BooksDataSet.class);
+		Criteria criteria = sessionFactory.getCurrentSession().createCriteria(BooksDataSet.class);
 		criteria.createAlias("author", "a");
 		criteria.add(Restrictions.eq("a.id", authorId));
 		return (ArrayList<BooksDataSet>) criteria.list();
@@ -51,7 +53,7 @@ public class BooksDAO implements BooksDAOI {
 
 	@Override
 	public ArrayList<BooksDataSet> getBooksByGenreId(long genreId) {
-		Criteria criteria = session.createCriteria(BooksDataSet.class);
+		Criteria criteria = sessionFactory.getCurrentSession().createCriteria(BooksDataSet.class);
 		criteria.createAlias("genre", "g");
 		criteria.add(Restrictions.eq("g.id", genreId));
 		return (ArrayList<BooksDataSet>) criteria.list();
@@ -59,31 +61,31 @@ public class BooksDAO implements BooksDAOI {
 
 	@Override
 	public AuthorsDataSet getAuthorById(long authorId) {
-		Criteria criteria = session.createCriteria(AuthorsDataSet.class);
+		Criteria criteria = sessionFactory.getCurrentSession().createCriteria(AuthorsDataSet.class);
 		criteria.add(Restrictions.idEq(authorId));
 		return (AuthorsDataSet) criteria.uniqueResult();
 	}
 
 	@Override
 	public GenresDataSet getGenreById(long genreId) {
-		Criteria criteria = session.createCriteria(GenresDataSet.class);
+		Criteria criteria = sessionFactory.getCurrentSession().createCriteria(GenresDataSet.class);
 		criteria.add(Restrictions.idEq(genreId));
 		return (GenresDataSet) criteria.uniqueResult();
 	}
 
 	@Override
 	public void addAuthor(AuthorsDataSet author) {
-		session.save(author);		
+		sessionFactory.getCurrentSession().save(author);		
 	}
 
 	@Override
 	public void addGenre(GenresDataSet genre) {
-		session.save(genre);	
+		sessionFactory.getCurrentSession().save(genre);	
 	}
 
 	@Override
 	public BooksDataSet getBookById(long bookId) {
-		Criteria criteria = session.createCriteria(BooksDataSet.class);
+		Criteria criteria = sessionFactory.getCurrentSession().createCriteria(BooksDataSet.class);
 		criteria.add(Restrictions.idEq(bookId));
 		return (BooksDataSet) criteria.uniqueResult();
 	}
